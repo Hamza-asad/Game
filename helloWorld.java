c21dce8452df026071a1268f0e007878fd0c25d2
Hamza-asad <Hamzaasad07861@gmail.com>
2020-07-23 21:27:36 +0200

    User input message

--- helloWorld.java
+++ helloWorld.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 
 public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.print("Hello World!");
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter your input here: ");
+		
+		String User = input.nextLine();
+		System.out.print(User);
 	}
 
 }
